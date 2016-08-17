a26724116dee2fa2d27cccc5a1e0a25a12f3c236
MariaRomero <fantasysystems@gmail.com>
2016-08-17 22:50:19 +0100

    added react router, another view and layout

---
+++ Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Login extends Component { {
+      return (
+          <div>
+            <form>
+              <input type="text"/>
+              <input type="password"/>
+            </form>
+          </div>
+      );
+    }
+}
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+import React from 'react';
 import { render } from 'react-dom';
 import Task from './Components/Task/Task';
 
-// const appEl = document.querySelector('#app');
 const appEl = document.getElementById('app');
 
-render(TaskItem, appEl);
+render(<Task name="buy milk"/>, appEl);
